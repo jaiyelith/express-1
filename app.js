c282cfe71ef275ff84b19405c0e6dc0d6304d840
jaiyelith <jaii_b@hotmail.com>
2023-09-20 16:15:33 -0500

    Se creó la ruta login

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 const app = express();
 
-const { validateHTTPMethods } = require("./http-method-middleware");
+dotenv.config();
+
+
+const users = [
+  {
+    username: "usuario1",
+    password: "contrasena1",
+  },
+  {
+    username: "usuario2",
+    password: "contrasena2",
+  },
+];
 
 app.use(express.json());
-app.use(validateHTTPMethods); 
 
-const listViewRouter = require("./list-view-router");
-const listEditRouter = require("./list-edit-router");
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find((u) => u.username === username && u.password === password);
 
-app.use("/list-view", listViewRouter);
-app.use("/list-edit", listEditRouter);
+  if (!user) {
+    return res.status(401).json({ message: "Credenciales incorrectas" });
+  }
+
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+  res.json({ token });
+});
+
+function authenticateToken(req, res, next) {
+  const token = req.header("Authorization");
+
+  if (!token) {
+    return res.status(401).json({ message: "Token de autorización no proporcionado" });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Token no válido" });
+    }
+    req.user = user;
+    next();
+  });
+}
+
+app.get("/protegida", authenticateToken, (req, res) => {
+  res.json({ message: "Ruta protegida. Usuario autenticado: " + req.user.username });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
